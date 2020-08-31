2ad9fe7220d04c5745b4de87a7a0843f40a027b2
woomin <dal96k@gmail.com>
2020-08-31 20:13:59 +0900

    Modify port for deploying heroku
    Heroku에 배포를 하기 위해서 포트가 환경변수를 통해 설정될 수 있도록
    수정하였습니다.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './pugs');
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}...`);
+app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
 });
