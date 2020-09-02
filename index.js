c733590623d64ee88db717de83c530154ed0bcaf
woomin <dal96k@gmail.com>
2020-09-02 17:26:47 +0900

    Refactor architecture (router, public)
    라우터의 최상위 위치가 루트의 index.js였는데 여기보다는 routes 디렉토리 안에
    index.js 파일을 만들고 여기서 관리하는 것이 더 명확한 것 같아서
    변경하였습니다. 아울러 express.static 역시 일반적으로 public 디렉토리로
    설정하고 여기서 HTML/CSS/JS 파일들을 관리하다는 점에서 저도 비슷하게
    하기 위해 express.static('public')으로 변경하고 CSS파일을
    이전하였습니다.

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
-app.use(express.static('views'));
+app.use(express.static('public'));
 
 app.use(express.json());
 
-app.use('/', require('./routes/page'));
-app.use('/user', require('./routes/user'));
+app.use('/', require('./routes/'));
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}...`);
