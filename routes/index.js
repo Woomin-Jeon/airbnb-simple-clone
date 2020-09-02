f36623ef70c0238299de9e4c14de069cb16720ef
woomin <dal96k@gmail.com>
2020-09-02 19:41:16 +0900

    Remove user route
    기존에는 서버를 api로 사용하였기 때문에 user라는 라우트가 필요했었지만
    서버사이드 렌더링 방식으로 변경하면서 모든 라우트는 렌더링을 하는 쪽으로
    하게 되었습니다. 이에 따라 user 라우트를 제거하고 페이지를 렌더링하는
    책임에 맞춰 page.js 파일로 관련 로직을 이동시켰습니다.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./page'));
-router.use('/user', require('./user'));
 
 module.exports = router;
--- routes/page.js
+++ routes/page.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const DB = require('../database/util');
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
 router.get('/signup', (req, res) => {
-  res.render('signup');
+  res.render('signup', { idDuplication: false });
+});
+
+router.post('/signup', async (req, res) => {
+  const { pwCheck, ...user } = req.body;
+  
+  const existingUser = await DB.findUserById(user.id);
+
+  if (existingUser) {
+    res.render('signup', { idDuplication: true });
+    return;
+  }
+
+  if (user.pw !== pwCheck) {
+    res.render('signup', { pwNotMatched: true });
+    return;
+  }
+
+  await DB.addUser(user);
+  res.redirect('/');
 });
 
 module.exports = router;
