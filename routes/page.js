9413f0eef4a3ab5c964b143c63eb0826fe2f5d1f
woomin <dal96k@gmail.com>
2020-09-02 21:25:35 +0900

    Add login route, modify root route
    로그인을 하면 유저정보를 확인하고 세션을 설정해주는 라우트를
    설정해주었습니다. 아울러 로그인한 유저가 홈으로 리다이렉트 되면 로그인
    되었음을 알릴 수 있도록, 즉 유저정보가 포함된 페이지를 렌더링 하도록
     루트 라우트의 내용도 약간 수정하였습니다.

--- routes/page.js
+++ routes/page.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const DB = require('../database/util');
+const useSHA256 = require('../util/encryption');
+const session = require('../session');
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/', async (req, res) => {
+  const userId = session.getIdBySession(req);
+
+  if (!userId) {
+    res.render('index', { loggedIn: false });
+    return;
+  }
+
+  const user = await DB.findUserById(userId);
+
+  res.render('index', { loggedIn: true, name: user.name });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.post('/login', async (req, res) => {
+  const { id, pw } = req.body;
+  const encryptedPassword = useSHA256(pw);
+
+  const user = await DB.findUserById(id);
+
+  if (!user) {
+    res.redirect('/');
+    return;
+  }
+
+  if (user.pw !== encryptedPassword) {
+    res.redirect('/');
+    return;
+  }
+
+  session.setSession(res, user.id);
+  res.redirect('/');
 });
 
 router.get('/signup', (req, res) => {
