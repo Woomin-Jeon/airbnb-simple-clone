5a91d789308d5f32e69ffd32bbca2eb6b36e3df1
woomin <dal96k@gmail.com>
2020-09-02 18:08:54 +0900

    Remove fetch logic
    마스터님이 fetch를 쓰지 지양하고 form을 사용하라고 하셨기 때문에
    fetch를 사용하여 비동기 통신해서 데이터를 서버로부터 받아온 뒤
    프론트에서 동적으로 처리하는 코드를 모두 제거하고, 오직 form으로
    데이터를 서버로 전송하고 서버사이드 렌더링을 사용하도록 로직을 변경하였습니다.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', require('./routes/'));
--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,12 @@ const router = express.Router();
 const DB = require('../database/util');
 
 router.post('/signup', async (req, res) => {
-  const user = req.body;
+  //TODO: Should check signup elements are correct
+  const { pwCheck, ...user } = req.body;
+  
   await DB.addUser(user);
 
-  res.status(200).send();
-});
-
-router.post('/signup/id-validation', async (req, res) => {
-  const { id } = req.body;
-  const user = await DB.findUserById(id);
-
-  if (user) {
-    res.status(400).send(false);
-    return;
-  }
-
-  res.status(200).send(true);
+  res.redirect('/');
 });
 
 module.exports = router;
