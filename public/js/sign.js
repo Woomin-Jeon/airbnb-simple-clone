8651e0598fe24d942978a7274bfa3c4f09964ae5
woomin <dal96k@gmail.com>
2020-09-07 21:52:28 +0900

    Add javascript code to modal close button
    아까 만들어둔 HTML/CSS 모달 닫기 버튼에 JS코드를 추가하여 실제로 클릭시
    닫히도록 만들었습니다. 아울러 로그인 창에서 회원가입 버튼을 누르면 회원가입
    화면으로 전환 되지 않는 문제를 발견하고, 클릭시 기존의 모달창은 닫은 뒤
    열도록 코드를 수정하였습니다.

--- public/js/sign.js
+++ public/js/sign.js
@@ -3,9 +3,20 @@ const $loginModal = document.getElementById('loginModal');
 const $logoutButton = document.getElementById('logoutButton');
 const $signupButton = document.getElementById('signupButton');
 const $signupModal = document.getElementById('signupModal');
+const $signupClose = document.getElementById('signupClose');
+const $loginClose = document.getElementById('loginClose');
+
+$loginClose.addEventListener('click', () => {
+  $loginModal.style.display = 'none';
+});
+
+$signupClose.addEventListener('click', () => {
+  $signupModal.style.display = 'none';
+});
 
 $loginButton.addEventListener('click', () => {
   $loginModal.style.display = 'block';
+  $signupModal.style.display = 'none';
 });
 
 $logoutButton.addEventListener('click', () => {
@@ -14,4 +25,5 @@ $logoutButton.addEventListener('click', () => {
 
 $signupButton.addEventListener('click', () => {
   $signupModal.style.display = 'block';
+  $loginModal.style.display = 'none';
 });
