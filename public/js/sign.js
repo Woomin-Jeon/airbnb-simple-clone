c6531952ad94c1d5bb1a78e1c1c38c36050ff743
woomin <dal96k@gmail.com>
2020-09-08 11:31:59 +0900

    Add screen dark logic to sign.js
    모달창을 띄우면 주변이 어두워지도록 하는 JS 코드를 추가하였고, 모달창을
    제거하면 다시 밝아지도록 하였습니다. 또한 기존의 hiddenAndShow라는
    함수는 더이상 사용하지 않았기 때문에 제거하였습니다. 마지막으로 모달창의
    외부를 클릭해도 모달창이 닫히도록 관련 이벤트도 추가하였습니다.

--- public/js/sign.js
+++ public/js/sign.js
@@ -5,17 +5,23 @@ const $signupButton = document.getElementById('signupButton');
 const $signupModal = document.getElementById('signupModal');
 const $signupClose = document.getElementById('signupClose');
 const $loginClose = document.getElementById('loginClose');
+const $backgroundDark = document.getElementById('background-dark');
 
 const move = (path) => window.location.href = path;
-const show = (dom) => dom.style.display = 'block';
-const hidden = (dom) => dom.style.display = 'none';
-const hiddenAndShow = (hiddenDom) => (showDom) => {
-  hidden(hiddenDom);
-  show(showDom);
-};
+const show = (dom) => {
+  dom.style.display = 'block';
+  $backgroundDark.style.display = 'block';
+}
+const hidden = (dom) => {
+  dom.style.display = 'none';
+  $backgroundDark.style.display = 'none';
+}
 
-$loginButton.addEventListener('click', () => hiddenAndShow($signupModal)($loginModal));
-$signupButton.addEventListener('click', () => hiddenAndShow($loginModal)($signupModal));
+$loginButton.addEventListener('click', () => show($loginModal));
+$signupButton.addEventListener('click', () => show($signupModal));
 $loginClose.addEventListener('click', () => hidden($loginModal));
 $signupClose.addEventListener('click', () => hidden($signupModal));
 $logoutButton.addEventListener('click', () => move('/logout'));
+$backgroundDark.addEventListener('click', () => {
+  [$backgroundDark, $loginModal, $signupModal].forEach(dom => hidden(dom));
+});
