a5ea907ef598a5a168dbdcaac32ed7bb30e38693
woomin <dal96k@gmail.com>
2020-09-08 17:17:06 +0900

    Add category selecting feature
    고객이 원하는 카테고리를 해당 카테고리에 맞는 예약 폼이 나오도록 기능을
    추가하였습니다.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,24 @@
 const $searchButton = document.getElementById('searchButton');
 const $userButton = document.getElementById('userButton');
 const $userButtonOn = document.getElementById('userButtonOn');
+const $categoryLocation = document.getElementById('category-location');
+const $categoryActivity = document.getElementById('category-activity');
+const $reservationActivity = document.getElementById('reservation-activity');
+const $reservationLocation = document.getElementById('reservation-location');
+
+$categoryActivity.addEventListener('click', () => {
+  $reservationLocation.style.display = 'none';
+  $reservationActivity.style.display = 'flex';
+  $categoryLocation.style.border = 'none';
+  $categoryActivity.style.borderBottom = '3px solid black';
+});
+
+$categoryLocation.addEventListener('click', () => {
+  $reservationLocation.style.display = 'flex';
+  $reservationActivity.style.display = 'none';
+  $categoryActivity.style.border = 'none';
+  $categoryLocation.style.borderBottom = '3px solid black';
+});
 
 $userButton.addEventListener('click', () => {
   const targetCss = $userButtonOn.style;
